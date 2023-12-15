9f1c4f858d0e42b33d6e41d86845034711fc1e1e
afonsolelis <afonsolelis@gmail.com>
2023-12-14 22:39:52 -0300

    feat: criacao do list controller

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,8 @@
+class ListController < ApplicationController
+  def invert
+    head_node = params[:list_head]
+    inverted_list = InvertListService.invert(head_node)
+    # Você pode ajustar a saída conforme necessário
+    render json: { inverted_list: }
+  end
+end
